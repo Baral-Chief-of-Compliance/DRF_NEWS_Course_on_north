94da4669269f825eeccdb6095b0e4c5e4d58cb17
unknown <dartmol21@mail.ru>
2023-10-25 16:40:45 +0300

    change in static dirs

--- admin_panel/settings.py
+++ admin_panel/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/staticfiels/'
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [STATIC_DIR]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiels')
