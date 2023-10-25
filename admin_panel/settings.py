5d8ecc82a84e747206a76bba42d393fd1b3df450
unknown <dartmol21@mail.ru>
2023-10-25 17:14:24 +0300

    add changes for media

--- admin_panel/settings.py
+++ admin_panel/settings.py
@@ -149,7 +149,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR,'public_html', 'media')
 
 CKEDITOR_UPLOAD_PATH = 'uploads/'
 
